2cd6571a1bf157a2a2dd85bf5112254e86bd3b27
sahiedu <sal.hid.edu@gmail.com>
2024-01-10 17:19:20 -0600

    New mode item added to the Number class in the mode.h file

--- ArduCopter/mode.h
+++ ArduCopter/mode.h
@@ -95,6 +95,7 @@ public:
         AUTOROTATE =   26,  // Autonomous autorotation
         AUTO_RTL =     27,  // Auto RTL, this is not a true mode, AUTO will report as this mode if entered to perform a DO_LAND_START Landing sequence
         TURTLE =       28,  // Flip over after crash
+        NEW_MODE =     29,  // New flight mode 
 
         // Mode number 127 reserved for the "drone show mode" in the Skybrush
         // fork at https://github.com/skybrush-io/ardupilot
